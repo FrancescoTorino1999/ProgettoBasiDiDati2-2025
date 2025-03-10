a7ee556101c6f62f4ebcff71ab8e9aff5bce009c
FrancescoTorino1999 <francesco.torino1999@gmail.com>
2025-03-10 20:56:07 +0100

    First commit

---
+++ Backend/basididati2/src/test/java/com/project/unisa/bd2/basididati2/Basididati2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.unisa.bd2.basididati2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Basididati2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
